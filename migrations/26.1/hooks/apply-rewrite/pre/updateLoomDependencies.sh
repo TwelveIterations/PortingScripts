c53919e88e12ce3c6bb6714bfc52e0eb4dfd05d6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-01-11 12:31:39 +0100

    Push changes made by hook script

--- migrations/26.1/hooks/apply-rewrite/pre/updateLoomDependencies.sh
+++ migrations/26.1/hooks/apply-rewrite/pre/updateLoomDependencies.sh
@@ -10,8 +10,12 @@ find . -name "*.gradle" | while read -r gradle_file; do
     sed -i 's/modRuntimeOnly(/runtimeOnly(/g' "$gradle_file"
     sed -i 's/modCompileOnly(/compileOnly(/g' "$gradle_file"
     sed -i 's/modLocalRuntime(/localRuntime(/g' "$gradle_file"
-    echo "Updated: $gradle_file"
+    echo "Processed: $gradle_file"
+    git add "$gradle_file"
 done
+
+git commit -m "Update Loom dependency configuration"
+git push origin $HOOKS_BRANCH
 
 echo "Done"
 
